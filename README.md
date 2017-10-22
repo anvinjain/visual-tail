1612a2a7bb1bac8eddc11020592e5a88b0a63447
fk-ajain <ankurjain.v@flipkart.com>
2017-10-23 01:03:54 +0530

    better name for http data source

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # Visual Tail
 
-Collection of visualizations over streaming data. Comes with default visualizations and http connector for streams.
-Code adapted from [Bogey](http://bogey.cognizo.com)
+Framework to connect datasources (ideally streaming ones), transform extracted data entries and feed them to visualizations which accept data events.
 
+> Code adapted from [Bogey](http://bogey.cognizo.com)
 
-### Pong visualization
+---
+
+### Included datasources
+
+#### Http Streaming Json
+Works on json objects sent over http stream (contiguous, not separated by any character)
+Supports running `fetch` in a continuous loop and handles streams on every chunk basis.
+
+---
+
+### Included visualizations
+
+#### Pong visualization
 Pong visualization adapted from [Bogey Pong](https://github.com/cognizo/bogey-pong) which in turn is heavily inspired by [Logstalgia](https://code.google.com/p/logstalgia/).
 
 Pong represents web server traffic as a giant pong game. Every request is a ball. Successful requests (200 status codes) are returned by the paddle, 400 status codes are misses and 500s are screen-shaking explosions. Requests are grouped together by IP address and floods of requests from the same client are represented as long streams of balls. This makes it really easy to pick out strange or suspicious traffic patterns.
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-//Init for phaser engine™¡
+//Init for phaser engine
 window.PIXI = require('phaser/build/custom/pixi');
 window.p2 = require('phaser/build/custom/p2');
 window.Phaser = require('phaser/build/custom/phaser-split');
 
+var $ = require('jquery');
 var Bogey = require('./lib/bogey');
 var Pong = require('./lib/vis/pong');
-var DgrepFetcher = require('./lib/ds/dgrep');
+var HttpStreamingJsonSource = require('./lib/datasource/http-streaming-json-source');
 var PongPayload = require('./lib/vis/pong/payload');
-var $ = require('jquery');
 
-var dgrepApi = "10.47.5.141";
+var apiEndpoint = "10.47.5.141";
 var appId = "fk-prof";
 var messageObj = "fk-prof-backend_request";
 var searchStr = "obj@" + messageObj;
@@ -18,7 +18,7 @@ var container = $('div.bogey-visualization-container').get(0);
 var bogey = new Bogey(container, null);
 Pong.run(bogey);
 
-var getCallback = function() {
+var messageHandler = function() {
     var handler = bogey.requestHandler();
     return function(entry) {
         switch(entry.type) {
@@ -39,7 +39,7 @@ var getCallback = function() {
     }
 }
 
-var transformer = function(msg) {
+var messageTransformer = function(msg) {
     if(msg.message == "obj" && msg.object[messageObj]) {
         var log = msg.object[messageObj];
         return new PongPayload(log.status, log.verb + " " + log.url, log.client_ip);
@@ -48,129 +48,6 @@ var transformer = function(msg) {
     return null;
 }
 
-var url = "http://" + dgrepApi + "/query?app_id=" + appId + "&substr=" + searchStr;
-var callback = getCallback();
-var fetcher = new DgrepFetcher(url, transformer, callback);
-fetcher.runWithRepeat();
-
-
-// fetcher.run().then(x => {
-//     console.log("hooba", x);
-// }).catch(err => {
-//     console.log("dooba");
-//     callback({type: "error", msg: "Error while doing fetch request " + this.url, err: err});
-// });
-// //Generate mock data
-// var counter = 1;
-// window.setInterval(function() {
-//     callback({
-//         parsed: {
-//             'ip': [ "ip" + counter ],
-//             'uri': [ "uri" + counter],
-//             'statusCode': [ "300" ]
-//         }
-//     });
-//     counter++;
-// }.bind(this), 250);
-
-
-// function parseLogMessage(msg) {
-//     if(msg.message == "obj" && msg.object["fkpbackend_request"]) {
-//         var log = msg.object["fkpbackend_request"];
-//         return {
-//             host: msg.host,
-//             host_ip: msg.sndr,
-//             parsed: {
-//                 ip: [ log.client_ip ],
-//                 uri: [ log.verb + " " + log.url ],
-//                 statusCode: [ log.status.toString() ]
-//             }
-//         }
-//     }
-//     return null;
-// }
-
-// function dgrepFetcher(url, callback) {
-//     fetch(url, {method: 'GET', mode: 'cors'}).then(response => {
-//         if(response.ok) {
-//             var reader = response.body.getReader();
-//             var decoder = new TextDecoder();
-//             var partial = false;
-//             function chunkHandler() {
-//                 return reader.read().then(result => {
-//                     if(result.done) {
-//                         console.log("Chunks are done, restart fetch");
-//                         return;
-//                     }
-//                     var chunk = decoder.decode(result.value || new Uint8Array, {stream: !result.done});
-//                     var startIdx = 0, msg;
-//                     if(partial) {
-//                         var partialEnd = chunk.indexOf("}{", startIdx);
-//                         if(partialEnd == -1) {
-//                             console.log("Another partial received in entirety, skipping chunk");
-//                             startIdx = chunk.length;
-//                         } else {
-//                             console.log("Partial dropped from the start of chunk");
-//                             startIdx = partialEnd + 1;
-//                         }
-//                     }
-//                     if(startIdx < chunk.length) {
-//                         if (chunk[startIdx] != '{') {
-//                             console.error("Invalid chunk, terminating processing of this request");
-//                             reader.cancel();
-//                             return;
-//                         }
-//                         while(true) {
-//                             if(startIdx == chunk.length) {
-//                                 console.log("Processing complete for chunk");
-//                                 break;
-//                             }
-//                             var msgEnd = chunk.indexOf("}{", startIdx);
-//                             if(msgEnd == -1) {
-//                                 try {
-//                                     msg = JSON.parse(chunk.substring(startIdx));
-//                                     partial = false;
-//                                     var payload = parseLogMessage(msg);
-//                                     if(payload) {
-//                                         callback(payload);
-//                                     } else {
-//                                         console.warn("Invalid log message received, skipping it");
-//                                     }
-//                                 } catch (err) {
-//                                     console.log("Invalid JSON, partial received at the end. Dropping it");
-//                                     partial = true;
-//                                 }
-//                                 startIdx = chunk.length;
-//                             } else {
-//                                 try {
-//                                     msg = JSON.parse(chunk.substring(startIdx, msgEnd + 1));
-//                                     var payload = parseLogMessage(msg);
-//                                     if(payload) {
-//                                         callback(payload);
-//                                     } else {
-//                                         console.warn("Invalid log message received, skipping it");
-//                                     }                               
-//                                 } catch (err) {
-//                                     console.error("Invalid JSON which was unexpected here. Terminating processing of this request");
-//                                     reader.cancel();
-//                                     return;
-//                                 }
-//                                 startIdx = msgEnd + 1;
-//                             }
-//                         }
-//                     }
-//                     return chunkHandler();
-//                 }).catch(err => {
-//                     console.error("Error reading chunks", err);
-//                 });
-//             }
-//             chunkHandler();
-//         } else {
-//             console.error("Non ok response received", response);
-//         }
-//     }).catch(err => {
-//         console.error("Error in connection", err);
-//     });
-// }
-
-// dgrepFetcher(url, callback);
+var url = "http://" + apiEndpoint + "/query?app_id=" + appId + "&substr=" + searchStr;
+var datasource = new HttpStreamingJsonSource(url, messageTransformer, messageHandler());
+datasource.runWithRepeat();
